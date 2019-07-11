aea1d790971e895fd78793febdb20faa5d9ddac0
kassandrashort <re.kassandrashort@gmail.com>
2019-07-11 13:42:11 -0600

    adding files to new repository

---
+++ crud.php
@@ -0,0 +1,99 @@
+<?php
+
+// include "read.php";
+$connection = mysqli_connect('localhost', 'root', '', 'dbthree');
+
+if(isset($_POST['submit'])) {
+    $username = $_POST['user_name'];
+    $password = $_POST['password'];
+
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
+
+    // $hashFormat = "$2y$10$";
+    // $salt = "iwritesomecrazystrings22";
+    // $hashF_and_salt = $hashFormat . $salt;
+
+    // $password = crypt($password, $hashF_and_salt);
+    // if($password){
+    //     echo "good";
+    // }
+
+    $query = "INSERT INTO updaters(name, password) ";
+    $query .= "VALUES ('$username', '$password')";
+
+    $results = mysqli_query($connection, $query);
+    if($results){
+        // echo "good";
+    } else {
+        echo "fail" . mysqli_error($connection);
+    }
+    $nameArr = ['cory', 'taysen', 'trevor', 'tyler', 'lindsey'];
+    if(in_array(strtolower($username), $nameArr)){
+        echo "Welcome $username, great name!";
+    } else {
+        echo "Welcome $username!";
+    }
+    if(empty($password)){
+        echo "Must input a password";
+    }
+
+}
+
+function datesOne() {
+    $month = date("F");
+    $dayofMonth = date("j");
+    $car = date("Y");
+    $time = date("g:i");
+    $year = date("Y");
+
+    echo "<br>Today is $month $dayofMonth, $year. <br><br>";
+}
+datesOne();
+
+function countDown() {
+    $weddingDate = mktime(0, 0, 0, 8, 16, 2019);
+    $currentTime = time();
+
+    $difference = $weddingDate - $currentTime;
+
+    $days = floor($difference / 84600);
+    $hours = $difference / 84600;
+    $minutes = floor($difference / 60);
+    $seconds = $difference; 
+
+    echo "There are $days days until the wedding!<br>";
+}
+
+countDown();
+
+?>
+
+<html>
+<head>
+<title>Wedding</title>
+<link href="main.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<form class="form1" action="crud.php" method="post">
+    <label for="user_name">Name</label>
+    <input type="text" name="user_name" id="user_name">
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password">
+    <a href="userinfo.php">
+    <input type="submit" name="submit" id="submit"> 
+    </a>
+    <a href="https://mail.google.com/mail/u/0/#inbox">Click ME</a>
+</form>
+
+<div id="link1">
+    <a href="read.php">DB Values</a>
+</div>
+
+</body>
+
+
+
+
+</html>
---
+++ read.php
@@ -0,0 +1,89 @@
+<?php
+include "update1.php";
+$connection = mysqli_connect('localhost', 'root', '', 'dbthree');
+
+mysqli_select_db($connection, "updaters");
+$query = "SELECT * FROM updaters";
+$result = mysqli_query($connection, $query);
+echo "<table id=table1 border=1>
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>Password</th>
+</tr>";
+while($row = mysqli_fetch_assoc($result)){
+    echo "<tr>";
+    echo "<td>" . "<input type=text value=" . $row['id'] . ">" . "</td>";
+    echo "<td>" . "<input type=text value=" . $row['name'] . ">" . "</td>";
+    echo "<td>" . "<input type=text value=" . $row['password'] . ">" . "</td>";
+    echo "<form action=read.php method=POST id=formUp>";
+    echo "<td>" . "<input type=submit form=formUP name=updateN class=button1 value=Edit" . ">" . "</td>";
+    echo "</form>";
+    echo "<td>" . "<input type=submit name=delete class=button1 value=Delete" . ">" . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+function updateNow(){
+if(isset($_POST['updateN'])){
+    // $username = $_POST['user_name'];
+    // $password = $_POST['password'];
+
+    // $query = "UPDATE users SET ";
+    // $query .= "name = '$username'";
+    // $query .= "password1 = '$password";
+    // $query .= "WHERE id = $ID";
+
+    // $results = mysqli_query($query, $connection);
+    echo "yes I do 666666";
+    }
+}
+
+?>
+
+<html>
+<head>
+<title>Wedding</title>
+<link href="main.css" rel="stylesheet" type="text/css">
+<!-- paletton.com -->
+<style>
+.button1 {
+    background-color: #6C81AF;
+    border-radius: 8px;
+}
+form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+#table1 td {
+    height: 40px;
+    padding: 8px;
+    margin: 8px;
+}
+input {
+    border-style: none;
+}
+.header {
+    <background-color: green;
+    height: 45px;
+    width: auto;
+}
+</style>
+</head>
+
+<body>
+    <div class=header>
+        <p>Header Text and image here</p>
+    </div>
+    <div id="link1">
+        <a href="crud.php">Home Page</a>
+    </div> 
+    <div>
+        <form action="read.php" method="post">
+            <input type="submit" value="yessss" name="updateN">
+        </form>
+    </div>
+</body>
+
+</html>
